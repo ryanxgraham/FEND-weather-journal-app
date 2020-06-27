1b087983cc9e0269b329919f3ba7e3b81aa2f078
ryanxgraham <ryanxgraham@gmail.com>
2020-06-27 13:40:03 -0500

    fixed indentation

--- server.js
+++ server.js
@@ -20,23 +20,23 @@ const port = 3000;
 
 // Setup Server
 const server = app.listen(port, listening);
- function listening(){
-    // console.log(server);
-    console.log(`running on localhost: ${port}`);
-  };
+function listening(){
+  // console.log(server);
+  console.log(`running on localhost: ${port}`);
+};
 
 // GET route
 app.get('/all', sendData);
 
 function sendData (request, response) {
-response.send(projectData);
+  response.send(projectData);
 };
 
 // POST route
 app.post('/add', callBack);
 
 function callBack(req,res){
-res.send('POST received');
+  res.send('POST received');
 };
 
 // POST an entry
